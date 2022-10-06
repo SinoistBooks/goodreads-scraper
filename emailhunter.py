6b861be11732c31db555d56087a841db23ce04b5
Dioni Zhong <dioniys@yahoo.com>
2022-10-06 12:56:31 +0100

    restructuring

--- emailhunter.py
+++ emailhunter.py
@@ -2,6 +2,9 @@ import html
 import re
 import requests
 
+from instaloader import Instaloader
+from instaloader.structures import Profile
+
 
 def get_domain(url):
     # get domain
@@ -11,41 +14,62 @@ def get_domain(url):
     return domain
 
 
-def search_emails_links(url):
+def get_emails(txt):
+    # get what looks like email from a given text
+    return set(re.findall('[\w\-\_]+@[\w\-\_]+\.[a-zA-Z]+', txt))
+
+
+def get_links(txt):
+    # get what looks like websites from a given text
+    links = re.findall('([http|https]+:\/\/[\w\-\.\/]+)', txt)
+    # get those with .com
+    coms = re.findall('([\w\-\.\/\:]+\.com[\w\-\/]+)', txt)
+    links.extend(coms)
+    # exclude truncated urls
+    links = [link for link in links if not link.endswith('...')]
+    # exclude goodreads site
+    links = [link for link in links if not re.match('.*goodreads.*', link)]
+    return set(links)
+
+
+def get_emails_links_by_url(url):
     try:
         r = requests.get(url)
         # print(f"status code: {r.status_code}")
     except requests.exceptions.ConnectionError:
         print(f'ERROR: Unable to reach url: {url}')
         return [], []
+    except requests.exceptions.MissingSchema:
+        # missing http:// so add and try again
+        url = f'http://{url}'
+        r = requests.get(url)
 
+    # unescape html characters
     txt = html.unescape(r.text)
 
     # find what looks like email
-    emails = re.findall('[\w\-\_]+@[\w\-\_]+\.[a-zA-Z]+', txt)
+    emails = get_emails(txt)
     if emails:
         return emails, []  # if there's email, no need to find links
 
-    # find all links
-    links = re.findall('[http|https]+:\/\/[\w\-\/.]+', txt)
-    links = [link.rstrip('/') for link in links]
-    links = set(links)
-    # for link in links:
-    #     print(link)
-
+    links = get_links(txt)
     print(f'Total {len(links)} links found.')
 
     return emails, links
 
 
-def get_emails(url):
+def hunt_emails(url):
+    """ Hunt emails by a given url.
+    It will try to find emails from the url, and from likely links found on 
+    the first url.
+    """
     print(f'\nSearch for emails from URL: {url}')
-    emails, links = search_emails_links(url)
+    emails, links = get_emails_links_by_url(url)
     if len(emails) >= 1:
         # got email(s), no need to look further
         return emails
 
-    # filter links
+    # filter links and get those with potential
     valid_links = []
     for link in links:
         # ignore the files
@@ -62,9 +86,40 @@ def get_emails(url):
         print('Potentially good links:')
         for link in valid_links:
             print(f' {link}')
-            emails, links = search_emails_links(link)
+            emails, _ = get_emails_links_by_url(link)
             if len(emails) >= 1:
                 # found emails!
                 return emails
 
     return []
+
+
+def get_insta_profile(url, loader):
+    """ 
+    loader: instance of Instaloader
+
+    Return: insta profile (dict) and emails (list)
+    """
+    m = re.match('.*instagram.com\/([\w]+)', url)
+    if not m:
+        print(f'ERROR: Unable to get instagram username from: {url}')
+        return None, []
+
+    username = m.groups()[0]
+    profile = Profile.from_username(loader.context, username)
+
+    insta = {}
+    insta['biography'] = profile.biography
+    insta['external_url'] = profile.external_url
+    insta['followers'] = profile.followers
+    insta['following'] = profile.followees
+
+    # check if there's email in bio
+    emails = get_emails(profile.biography)
+    if len(emails) >= 1:
+        # got email(s), no need to look further
+        return insta, emails
+
+    # if not found, search in the external url
+    emails, _ = get_emails_links_by_url(profile.external_url)
+    return insta, emails
